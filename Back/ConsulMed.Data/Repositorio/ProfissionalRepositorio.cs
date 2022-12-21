2c28af57cc6ed2342ee7739740fa81d5843c7c71
Ana Teresa <ana.t.negri.silva@avanade.com>
2022-12-21 18:12:55 -0300

    atualização arquivos profissional

--- Back/ConsulMed.Data/Repositorio/ProfissionalRepositorio.cs
+++ Back/ConsulMed.Data/Repositorio/ProfissionalRepositorio.cs
@@ -1,4 +1,6 @@
-﻿using ConsulMed.Data.Interface;
+﻿using ConsulMed.Data.Dto;
+using ConsulMed.Data.Entidade;
+using ConsulMed.Data.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,95 @@ namespace ConsulMed.Data.Repositorio
 {
     public class ProfissionalRepositorio : IProfissionalRepositorio
     {
+        private readonly Contexto.ConsulMedContext _contexto;
+
+        public ProfissionalRepositorio(Contexto.ConsulMedContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public int AtualizarProfissional(ProfissionalDto cadastrarDto)
+        {
+            Entidade.Profissional profissionalEntidadeBanco =
+                (from c in _contexto.Profissional
+                 where c.IdProfissional == cadastrarDto.IdProfissional
+                 select c)
+                 ?.FirstOrDefault()
+                 ?? new Entidade.Profissional();
+
+            if (profissionalEntidadeBanco == null || DBNull.Value.Equals(profissionalEntidadeBanco.IdProfissional) || profissionalEntidadeBanco.IdProfissional == 0)
+            {
+                return 0;
+            }
+
+            profissionalEntidadeBanco.Nome = cadastrarDto.Nome;
+            profissionalEntidadeBanco.Telefone = cadastrarDto.Telefone;
+            profissionalEntidadeBanco.Endereco = cadastrarDto.Endereco;
+            profissionalEntidadeBanco.Ativo = cadastrarDto.Ativo;
+
+            _contexto.ChangeTracker.Clear();
+            _contexto.Profissional.Update(profissionalEntidadeBanco);
+            return _contexto.SaveChanges();
+        }
+
+        public int CadastrarProfissional(ProfissionalDto cadastrarDto)
+        {
+            Entidade.Profissional profissionalEntidade = new Entidade.Profissional()
+            {
+                Nome = cadastrarDto.Nome,
+                Telefone = cadastrarDto.Telefone,
+                Endereco = cadastrarDto.Endereco,
+                Ativo = cadastrarDto.Ativo,
+            };
+
+            _contexto.ChangeTracker.Clear();
+            _contexto.Profissional.Add(profissionalEntidade);
+            return _contexto.SaveChanges();
+        }
+
+        public int ExcluirProfissional(int idProfissional)
+        {
+            Entidade.Profissional profissionalEntidadeBanco =
+                 (from c in _contexto.Profissional
+                  where c.IdProfissional == idProfissional
+                  select c).FirstOrDefault();
+
+            if (profissionalEntidadeBanco == null || DBNull.Value.Equals(profissionalEntidadeBanco.IdProfissional) || profissionalEntidadeBanco.IdProfissional == 0)
+            {
+                return 0;
+            }
+
+            _contexto.ChangeTracker.Clear();
+            _contexto.Profissional.Remove(profissionalEntidadeBanco);
+            return _contexto.SaveChanges();
+        }
+
+        public List<Dto.ProfissionalDto> ListarTodosProfissionais()
+        {
+            return _contexto.Profissional.Select(s => new Dto.ProfissionalDto()
+            {
+                IdProfissional = s.IdProfissional,
+                Nome = s.Nome,
+                Telefone = s.Telefone,
+                Endereco = s.Endereco,
+                Ativo = s.Ativo
+            }).ToList();
+        }
+
+        public ProfissionalDto ConsultarProfissionalPorId(int id)
+        {
+            return (from t in _contexto.Profissional
+                    where t.IdProfissional == id
+                    select new Dto.ProfissionalDto()
+                    {
+                        IdProfissional = t.IdProfissional,
+                        Nome = t.Nome,
+                        Telefone = t.Telefone,
+                        Endereco = t.Endereco,
+                        Ativo = t.Ativo
+                    })
+                    ?.FirstOrDefault()
+                    ?? new ProfissionalDto();
+        }
     }
 }
